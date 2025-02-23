0b6f8af8b7ad4f70605f3fece8019c028c417fbb
David Little <david.frank.little@gmail.com>
2025-02-22 23:34:55 -0500

    initial sketch of doc result filtering

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,8 @@ fn main() {
         in_comment: false,
         start_comment,
         end_comment,
-        comment_prefix
+        comment_prefix,
+        line_count: 0
     };
     for line_result in reader.lines() {
         let line = match line_result {
@@ -40,7 +41,7 @@ fn main() {
         };
         let filtered = comment_filter.apply(line.as_ref());
         if let Some(cline) = filtered {
-            println!("{}", cline);
+            println!("{}", cline.line);
         }
     }
 }
@@ -49,29 +50,110 @@ struct CommentFilter {
     in_comment: bool,
     comment_prefix: Regex,
     start_comment: Regex,
-    end_comment: Regex
+    end_comment: Regex,
+    line_count: i64,
+}
+
+struct CommentResult<'a> {
+    line: &'a str,
+    line_num: i64,
 }
 
 impl CommentFilter {
-    fn apply<'a>(&mut self, line: &'a str) -> Option<&'a str> {
+    fn apply<'a>(&mut self, line: &'a str) -> Option<CommentResult<'a>> {
         if !self.in_comment && self.start_comment.is_match(line) {
             self.in_comment = true;
+            self.line_count = 0;
             return None;
         } else if self.in_comment && self.end_comment.is_match(line) {
             self.in_comment = false;
-            return Some("\n");
+            let result = Some(CommentResult{
+                line: "\n",
+                line_num: self.line_count+1
+            });
+            self.line_count = 0;
+            return result;
         }
         if self.in_comment {
+            self.line_count += 1;
             let maybe_cap = self.comment_prefix.captures(line);
             if let Some(capture) = maybe_cap {
                 if let Some(cap_match) = capture.get(1) {
-                    return Some(cap_match.as_str());
+                    return Some(CommentResult{
+                        line: cap_match.as_str(),
+                        line_num: self.line_count,
+                    });
                 }
             }
-            return Some(line);
+            return Some(CommentResult{
+                line: line,
+                line_num: self.line_count,
+            });
         } else {
             return None
         }
+    }
+}
+
+struct DocFilter {
+    file_field: Regex,
+    order_field: Regex,
+    pending_result: Option<DocResult>,
+}
+
+struct DocResult<'a> {
+    file: &'a str,
+    order: i64,
+    body: String,
+    line_count: i64,
+}
+
+impl DocFilter {
+    fn apply<'a>(&mut self, comment: &'a CommentResult) -> Option<DocResult<'a>> {
+        let old_result = self.pending_result;
+        if comment.line_num == 1 {
+            if let Some(cap) = self.file_field.captures(comment.line) {
+                if let Some(cmatch) = cap.get(1) {
+                    self.pending_result = Some(DocResult {
+                        file: cmatch.as_str(),
+                        order: 0,
+                        body: String::new(),
+                        line_count: comment.line_num,
+                    });
+                }
+            }
+            self.pending_result = None;
+        }
+        if comment.line_num == 2 {
+            if let Some(cap) = self.order_field.captures(comment.line) {
+                if let Some(cmatch) = cap.get(1) {
+                    let order = match cmatch.as_str().parse::<i64>() {
+                        Ok(order) => order,
+                        Err(_) => {
+                            println!("Non-numeric order ({}), ignoring.", comment.line);
+                            0
+                        }
+                    }
+                    let old_result = self.pending_result.unwrap();
+                    self.pending_result = Some(DocResult {
+                        file: old_result.file,
+                        order: old_result.order,
+                        body: old_result.body,
+                        line_count: comment.line_num,
+                    });
+                }
+            }
+            self.pending_result = None;
+        }
+
+        // TODO: rather than returning an Option filters should
+        // return a `Filtered` value that can be Some, None or Final
+        if let Some(x) = old_result {
+            if let Some(y) = self.pending_result {
+                return self.pending_result;
+            }
+        }
+        return None
     }
 }
 
