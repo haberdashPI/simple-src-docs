278058957d0c7b241925e821d55e2c7bcb429840
David Little <david.frank.little@gmail.com>
2025-02-26 23:48:19 -0500

    CLI and file writing

---
+++ README.md
@@ -0,0 +1,52 @@
+# `simple-src-docs`
+
+simple-src-docs is an extremely bare bones tool to facilitate generating documentation from your source files.
+
+```
+❯ simple-src-docs --help
+Extracts doc strings into markdown files
+
+Takes all passed files in lexicographic order and searches for comments that start with
+`--start-comment` regex, and ending with `--end-comemnt` regex. Looks for `@file [file]`
+on the line following `--start-comment` and if present the contents of the comment are
+appended to the specified file path. The file and its directories are created at the
+given `--dest`. Optionally, after the line with `@file` you can provide `@order [num]` to
+influence the ordering of the comment content. Content is sorted from the lowest to the
+highest `order`, breaking ties by pre-sorted ordering.
+
+Usage: simple-src-docs [OPTIONS] [FILES]...
+
+Arguments:
+  [FILES]...
+
+
+Options:
+  -s, --start-comment <START_COMMENT>
+          [default: ^\s*/\*\*\s*$]
+
+  -e, --end-comment <END_COMMENT>
+          [default: ^\s*\*/\s*$]
+
+  -c, --comment-prefix <COMMENT_PREFIX>
+          [default: ^\s*\*+\s*(.*)$]
+
+  -d, --dest <DEST>
+          [default: .]
+
+  -h, --help
+          Print help (see a summary with '-h')
+
+  -V, --version
+          Print version
+```
+
+## Why?
+
+This could easily have been written in just about any language or as a set of calls to a few command line utilities. It's a simple tool with few features. I'm planning a bigger project in rust, and since I haven't written much rust yet, I wanted to start with a simple project to get some practical experience before diving into something a little more ambitious. The tool also happens to be useful for my current project. I don't have any plans to expand the scope of this utility.
+
+## Roadmap
+
+1. make it work
+2. write up some *very* basic unit tests
+3. Write up some CI for test
+4. Write up some CI for generating releases
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,123 @@
 use either::{Either, Left, Right};
 use regex::Regex;
 use core::fmt;
-use std::env;
+use std::fs;
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Write};
+use std::path::PathBuf;
+use std::collections::HashMap;
+use clap::Parser;
 
-// use std::collections::HashMap;
+/// Extracts doc strings into markdown files
+///
+/// Takes all passed files in lexicographic order and searches for comments that start with
+/// `--start-comment` regex, and ending with `--end-comemnt` regex. Looks for `@file [file]`
+/// on the line following `--start-comment` and if present the contents of the comment are
+/// appended to the specified file path. The file and its directories are created at the
+/// given `--dest`. Optionally, after the line with `@file` you can provide `@order [num]`
+/// to influence the ordering of the comment content. Content is sorted from the lowest to
+/// the highest `order`, breaking ties by pre-sorted ordering.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about)]
+struct Args {
+    // regex for the starting comment delimiter
+    #[arg(short, long, default_value = r"^\s*/\*\*\s*$")]
+    start_comment: String,
+    // regex for the ending comment delimiter
+    #[arg(short, long, default_value = r"^\s*\*/\s*$")]
+    end_comment: String,
+    // the prefix to be removed from each line between the start and end comment delimiter
+    #[arg(short, long, default_value = r"^\s*\*+\s*(.*)$")]
+    comment_prefix: String,
+
+    // root directory where markdown files are generated
+    #[arg(short, long, default_value = ".")]
+    dest: PathBuf,
+
+    // the files to extract comments from
+    files: Vec<String>,
+}
+
+fn to_regex(x: &str) -> Regex {
+    return match Regex::new(x) {
+        Ok(x) => x,
+        Err(e) => {
+            eprintln!("Invalid regex {}", e);
+            std::process::exit(1);
+        }
+    }
+}
+
 fn main() {
     let file_field = Regex::new(r"@file\s(.*)").unwrap();
     let order_field = Regex::new(r"@order\s(.*)").unwrap();
-
-    // TODO: make these command line arguments
-    let start_comment = Regex::new(r"^\s*/\*\*\s*$").unwrap();
-    let end_comment = Regex::new(r"^\s*\*/\s*$").unwrap();
-    let comment_prefix = Regex::new(r"^\s*\*+\s*(.*)$").unwrap();
-
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Required filename argument missing.");
+    let args = Args::parse();
+    let destination = args.dest;
+    if !destination.exists() {
+        eprintln!("The destination path `{}` does not exist.", destination.to_str().unwrap());
         std::process::exit(1);
     }
-    let filename = &args[1];
-    println!("File name is: {}.", filename);
-    let file = match File::open(filename) {
-        Ok(f) => f,
-        Err(e) => {
-            println!("Error reading file: {}", e);
-            std::process::exit(1);
+    let start_comment = to_regex(&args.start_comment);
+    let end_comment = to_regex(&args.end_comment);
+    let comment_prefix = to_regex(&args.comment_prefix);
+
+    let mut docmap = HashMap::new();
+    for file in args.files {
+        let io = match File::open(file) {
+            Ok(x) => x,
+            Err(e) => {
+                eprintln!("Error reading file: {}", e);
+                std::process::exit(1)
+            }
+        };
+        let reader = io::BufReader::new(io);
+        let str_lines = reader.lines().map(|x| match x {
+            Ok(r) => r,
+            Err(e) => {
+                eprintln!("Error reading file: {}", e);
+                std::process::exit(1)
+            }
+        });
+        let comments = Comments::new(str_lines, start_comment.clone(), end_comment.clone(), comment_prefix.clone());
+        let docs = DocIterator::new(comments, file_field.clone(), order_field.clone());
+        for doc in docs {
+            let items = docmap.entry(doc.file).or_insert(Vec::new());
+            items.push((doc.order, doc.body));
         }
-    };
-    let reader = io::BufReader::new(file);
-    let str_lines = reader.lines().map(|x| match x {
-        Ok(r) => r,
-        Err(e) => {
-            println!("Error reading file: {}", e);
-            std::process::exit(1)
+    }
+
+    for (file, items) in &docmap {
+        println!("Writing to doc file: {}", file);
+        let path = destination.join(file);
+        let dir = path.parent().unwrap();
+
+        match fs::create_dir_all(dir) {
+            Ok(x) => x,
+            Err(e) => {
+                eprintln!("Could not create directory {}", e);
+                std::process::exit(1);
+            }
         }
-    });
-    let comments = Comments::new(str_lines, start_comment, end_comment, comment_prefix);
-    let docs = DocIterator::new(comments, file_field, order_field);
-    for doc in docs {
-        println!("{}", doc);
+
+        let mut io = match File::create(&path) {
+            Ok(x) => x,
+            Err(e) => {
+                eprintln!("Could not create path {}", e);
+                std::process::exit(1);
+            }
+        };
+
+        let mut sorted = items.clone();
+        sorted.sort_by_key(|x| x.0);
+        for (_, body) in sorted {
+            match writeln!(io, "{}", body) {
+                Err(e) => {
+                    eprintln!("Failed to write {}", e);
+                    std::process::exit(1);
+                }
+                Ok(_) => ()
+            }
+        }
     }
 }
 
@@ -75,6 +152,7 @@ impl<T: Iterator<Item = String>> Comments<T> {
 struct CommentResult {
     value: String,
     line: i64,
+    last: bool,
 }
 
 impl<T: Iterator<Item = String>> Iterator for Comments<T> {
@@ -90,9 +168,14 @@ impl<T: Iterator<Item = String>> Iterator for Comments<T> {
             self.line_count = 0;
             return self.next();
         } else if self.in_comment && self.end_comment.is_match(value.as_str()) {
+            let result = Some(CommentResult {
+                value: String::new(),
+                line: self.line_count,
+                last: true
+            });
             self.in_comment = false;
             self.line_count = 0;
-            return self.next();
+            return result;
         }
         if self.in_comment {
             self.line_count += 1;
@@ -102,12 +185,14 @@ impl<T: Iterator<Item = String>> Iterator for Comments<T> {
                     return Some(CommentResult {
                         value: String::from(cap_match.as_str()),
                         line: self.line_count,
+                        last: false,
                     });
                 }
             }
             return Some(CommentResult {
                 value,
                 line: self.line_count,
+                last: false,
             });
         } else {
             return self.next();
@@ -179,7 +264,7 @@ impl<T: Iterator<Item = String>> DocIterator<T> {
             Some(num_str) => match num_str.parse() {
                 Ok(order) => return Left(order),
                 Err(_) => {
-                    println!("Non-numeric `@order` value ({}), ignoring.", comment.value,);
+                    println!("Non-numeric `@order` value `{}`, ignoring.", num_str);
                     return Left(0);
                 }
             },
@@ -208,7 +293,8 @@ impl<T: Iterator<Item = String>> Iterator for DocIterator<T> {
             }
         };
 
-        while let Some(comment) = self.comments.next() {
+        for comment in &mut self.comments {
+            if comment.last { break }
             body.push_str(&comment.value);
             body.push('\n');
         }
