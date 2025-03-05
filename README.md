be8ec85c746351197c7d33a13d1df00ab3602b72
David Little <david.frank.little@gmail.com>
2025-03-05 11:47:57 -0500

    Support configurable doc templates

--- README.md
+++ README.md
@@ -3,35 +3,50 @@
 simple-src-docs is an extremely bare bones tool to facilitate generating documentation from your source files.
 
 ```
-❯ simple-src-docs --help
 Extracts doc strings into markdown files
 
-Walks through all files in `[SOURCE]` and searches for comments that start with `--start-
-comment` regex, and ending with `--end-comemnt` regex. Looks for `@file [file]` on the line
-following `--start-comment` and if present the contents of the comment are appended to the
-specified file path. The file and its directories are created at the given `--dest`.
-Optionally, after the line with `@file` you can provide `@order [num]` to influence the
-ordering of the comment content. Content is sorted from the lowest to the highest `order`,
-breaking ties by pre-sorted ordering.
+Walks through all files in `<SOURCE>` and searches for comments that start with `<START_COMMENT>` regex, and
+ending with `<END_COMEMNT>` regex. Looks for `@file [file]` on the line following `<START_COMMENT>` and if
+present the contents of the comment are appended to the specified file path. The file and its directories are
+created at the given `<DEST>`. Optionally, after the line with `@file` you can provide `@order [num]` to
+influence the ordering of the comment content. Content is sorted from the lowest to the highest `order`,
+breaking ties by pre-sorted ordering. Additional `@` prefixed tags will be ignored in the output, unless you
+define an appropriate configuration template (See README.md for details)
 
 Usage: simple-src-docs [OPTIONS] <SOURCE>
 
 Arguments:
   <SOURCE>
-
+          the source directory where comments will be extracted from
 
 Options:
   -s, --start-comment <START_COMMENT>
+          regex for the starting comment delimiter
+
           [default: ^\s*/\*\*\s*$]
 
   -e, --end-comment <END_COMMENT>
+          regex for the ending comment delimiter
+
           [default: ^\s*\*/\s*$]
 
   -c, --comment-prefix <COMMENT_PREFIX>
+          the prefix to be removed from each comment line between the start and end comment delimiter; the
+          first capture group should denote the prefix, and the second the text to read
+
           [default: ^\s*\*+\s*(.*)$]
 
   -d, --dest <DEST>
+          root directory where markdown files are generated
+
           [default: .]
+
+  -v, --verbose
+          if set, show detailed messages about document processing
+
+      --config <CONFIG>
+          location of file used to further configure simple-src-docs (see README.md), defaults to
+          `<DEST>/.simple-src-docs.config.toml
 
   -h, --help
           Print help (see a summary with '-h')
@@ -40,13 +55,69 @@ Options:
           Print version
 ```
 
-## Why?
+The config file is a TOML file with the following sections:
 
-This could easily have been written in just about any language or as a set of calls to a few command line utilities. It's a simple tool with few features. I'm planning a bigger project in rust, and since I haven't written much rust yet, I wanted to start with a simple project to get some practical experience before diving into something a little more ambitious. The tool also happens to be useful for my current project. I don't have any plans to expand the scope of this utility.
+- `header`: must contain a `version` string that is [semver](https://semver.org/) compatible with 0.1
+- `doc`: Generates additional docs. Has three fields:
+  - `file`: the file to render docs to
+  - `order`: (optional), as per the `@order` field in comments, this
+    determines what order the body will be inserted into the file
+  - `body`: The text to insert into `file`
+- `template`: Transforms docs using the given set of tags into the `output` docs.
+  Can use additional arbitrary `@` prefixed tags listed in a doc
+  - `tags`: an array of strings; for the template to be applied these tags must
+    be present
+  - `output`: an array of objects with the following fields. All values
+    can be a [mustache template](https://mustache.github.io/), and
+    the value of any of the tags in the input doc section will be
+    inserted into this template.
+        - `file`: the file to store output in
+        - `order`: the ordering of the doc in the file
+        - `body`: the content to write to the file
 
-## Roadmap
 
-1. make it work
-2. write up some *very* basic unit tests
-3. Write up some CI for test
-4. Write up some CI for generating releases
+Example config file
+
+```toml
+[header]
+version = 0.1
+
+[[doc]]
+file = ".vitpress/commands.mjs"
+order = -1000
+body = """
+export default commandLines = [
+"""
+
+[[doc]]
+file = ".vitepress/commands.mjs"
+order = 10000
+body = """
+];
+"""
+
+[[template]]
+tags = ["command"]
+
+[[template.output]]
+file = ".vitepress/commands.mjs"
+order = "{{order}}"
+body = """
+{ text: '{{command}}', link: '/commands/{{command}}' },
+"""
+
+[[template.output]]
+file = "commands/index.md"
+order = "{{order}}"
+body = """
+ - [`master-key.{{command}}'](/commands/{{command}})
+"""
+
+[[template.output]]
+file = "commands/{{command}}.md"
+body = """
+# `master-key.{{command}}`
+
+{{__body__}}
+"""
+```
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
+use clap::Parser;
 use either::{Either, Left, Right};
-use std::cmp::Ordering;
+use mustache;
+use mustache::MapBuilder;
 use regex::Regex;
-use core::fmt;
+use serde::Deserialize;
+use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::fs;
 use std::fs::File;
-use std::io::{self, BufRead, Write};
-use std::path::PathBuf;
-use std::collections::HashMap;
-use clap::Parser;
+use std::io;
+use std::io::{BufRead, Write};
+use std::num::ParseFloatError;
+use std::path::{Path, PathBuf};
+use std::process::ExitCode;
+use validator::{Validate, ValidationError};
+use semver::{Version,VersionReq};
 use walkdir::WalkDir;
 
 /// Extracts doc strings into markdown files
@@ -18,7 +25,9 @@ use walkdir::WalkDir;
 /// appended to the specified file path. The file and its directories are created at the
 /// given `<DEST>`. Optionally, after the line with `@file` you can provide `@order [num]` to
 /// influence the ordering of the comment content. Content is sorted from the lowest to the
-/// highest `order`, breaking ties by pre-sorted ordering.
+/// highest `order`, breaking ties by pre-sorted ordering. Additional `@` prefixed
+/// tags will be ignored in the output, unless you define an appropriate configuration
+/// template (See README.md for details)
 #[derive(Parser, Debug)]
 #[command(version, about, long_about)]
 struct Args {
@@ -29,13 +38,23 @@ struct Args {
     #[arg(short, long, default_value = r"^\s*\*/\s*$")]
     end_comment: String,
     /// the prefix to be removed from each comment line between the start and end comment
-    /// delimiter
+    /// delimiter; the first capture group should denote the prefix, and the second the text
+    /// to read
     #[arg(short, long, default_value = r"^\s*\*+\s*(.*)$")]
     comment_prefix: String,
 
     /// root directory where markdown files are generated
     #[arg(short, long, default_value = ".")]
     dest: PathBuf,
+
+    /// if set, show detailed messages about document processing
+    #[arg(short, long)]
+    verbose: bool,
+
+    /// location of file used to further configure simple-src-docs
+    /// (see README.md), defaults to `<DEST>/.simple-src-docs.config.toml
+    #[arg(long)]
+    config: Option<PathBuf>,
 
     /// the source directory where comments will be extracted from
     source: PathBuf,
@@ -48,88 +67,292 @@ fn to_regex(x: &str) -> Regex {
             eprintln!("Invalid regex {}", e);
             std::process::exit(1);
         }
+    };
+}
+
+struct SrcDocError {
+    msg: String,
+    code: std::process::ExitCode,
+}
+
+impl SrcDocError {
+    fn new(msg: String) -> SrcDocError {
+        return SrcDocError {
+            msg,
+            code: ExitCode::FAILURE,
+        };
     }
 }
 
-fn main() {
+fn exit_code(x: Result<(), SrcDocError>) -> ExitCode {
+    match x {
+        Ok(_) => {
+            println!("Successfully generated documentation.");
+            return ExitCode::SUCCESS;
+        }
+        Err(e) => {
+            eprintln!("{}", e.msg);
+            return e.code;
+        }
+    };
+}
+
+fn main() -> ExitCode {
+    return exit_code(run());
+}
+
+impl From<io::Error> for SrcDocError {
+    fn from(e: io::Error) -> SrcDocError {
+        return SrcDocError::new(format!("IO Error: {}", e));
+    }
+}
+
+impl From<toml::de::Error> for SrcDocError {
+    fn from(e: toml::de::Error) -> SrcDocError {
+        return SrcDocError::new(format!("Config Error: {}", e));
+    }
+}
+
+impl From<walkdir::Error> for SrcDocError {
+    fn from(e: walkdir::Error) -> SrcDocError {
+        return SrcDocError::new(format!("Error traversing directories: {}", e));
+    }
+}
+
+fn run() -> Result<(), SrcDocError> {
     let file_field = Regex::new(r"@file\s(.*)").unwrap();
     let order_field = Regex::new(r"@order\s(.*)").unwrap();
     let args = Args::parse();
     let destination = args.dest;
     if !destination.exists() {
-        eprintln!("The destination path `{}` does not exist.", destination.to_str().unwrap());
-        std::process::exit(1);
+        return Err(SrcDocError::new(format!(
+            "The destination path `{}` does not exist.",
+            destination.display()
+        )));
     }
     let start_comment = to_regex(&args.start_comment);
     let end_comment = to_regex(&args.end_comment);
     let comment_prefix = to_regex(&args.comment_prefix);
 
+    let config = match args.config {
+        Some(x) => SrcDocConfig::from(x)?,
+        None => {
+            let default_config = destination.join(".simple-src-docs.config.toml");
+            if default_config.is_file() {
+                SrcDocConfig::from(default_config)?
+            } else {
+                SrcDocConfig::new()
+            }
+        }
+    };
+
     let mut docmap = HashMap::new();
     for entry in WalkDir::new(args.source) {
-        let file_entry = match entry {
-            Ok(p) => p,
-            Err(e) => {
-                eprintln!("Error walking paths: {}", e);
-                continue;
-            }
-        };
+        let file_entry = entry?;
         let file = file_entry.path();
         if !file.is_file() { continue; }
 
-        let io = match File::open(file) {
-            Ok(x) => x,
-            Err(e) => {
-                eprintln!("Error reading file: {}", e);
-                std::process::exit(1)
-            }
-        };
+        let io = File::open(file)?;
         let reader = io::BufReader::new(io);
-        let str_lines = reader.lines().map(|x| match x {
-            Ok(r) => r,
-            Err(e) => {
-                eprintln!("Error reading file: {}", e);
-                std::process::exit(1)
-            }
-        });
-        let comments = Comments::new(str_lines, start_comment.clone(), end_comment.clone(), comment_prefix.clone());
+        let str_lines = reader.lines().map_while(Result::ok);
+        if args.verbose { println!("Reading file {}", file.to_str().unwrap()); }
+        let comments  = Comments::new(
+            str_lines,
+            start_comment.clone(),
+            end_comment.clone(),
+            comment_prefix.clone(),
+        );
         let docs = DocIterator::new(comments, file_field.clone(), order_field.clone());
         for doc in docs {
-            let items = docmap.entry(doc.file).or_insert(Vec::new());
-            items.push((doc.order, doc.body));
+            config.apply(doc, &mut docmap)?;
         }
     }
 
-    println!("Writing doc files:");
-    for (file, items) in &docmap {
-        println!(" - {}", file);
+    for doc in config.doc.unwrap_or_default() {
+        let items = docmap.entry(doc.file).or_insert(Vec::new());
+        items.push((doc.order, doc.body));
+    }
+
+    if args.verbose { println!("Writing doc files:"); }
+    for (file, items) in docmap.iter_mut() {
+        if args.verbose {
+            println!(" - {}", file);
+        }
         let path = destination.join(file);
         let dir = path.parent().unwrap();
 
-        match fs::create_dir_all(dir) {
-            Ok(x) => x,
-            Err(e) => {
-                eprintln!("Could not create directory {}", e);
-                std::process::exit(1);
-            }
-        }
+        fs::create_dir_all(dir)?;
+        let mut io = File::create(&path)?;
 
-        let mut io = match File::create(&path) {
-            Ok(x) => x,
-            Err(e) => {
-                eprintln!("Could not create path {}", e);
-                std::process::exit(1);
+        items.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap_or(Ordering::Less));
+        for (_, body) in items {
+            write!(io, "{}", body)?;
+        }
+    }
+    return Ok(());
+}
+
+// Templates ///////////////////////////////////////////////////////////////////////////////
+
+#[derive(Deserialize)]
+struct SrcDocConfig {
+    header: ConfigHeader,
+    #[serde(default)]
+    template: Option<Vec<DocTemplate>>,
+    #[serde(default)]
+    doc: Option<Vec<DocResult>>,
+}
+
+impl SrcDocConfig {
+    fn new() -> SrcDocConfig {
+        return SrcDocConfig {
+            header: ConfigHeader {
+                version: Version::parse("0.1").unwrap(),
+            },
+            template: None,
+            doc: None,
+        };
+    }
+
+    fn from<T: AsRef<Path>>(path: T) -> Result<SrcDocConfig, SrcDocError> {
+        let str = fs::read_to_string(&path)?;
+        let mut result = toml::from_str::<SrcDocConfig>(&str)?;
+        result.template.iter_mut().for_each(|x|
+            x.sort_by(|a, b| a.order.partial_cmp(&b.order).unwrap_or(Ordering::Less)));
+        return Ok(result);
+    }
+}
+
+fn valid_version(v: &Version) -> Result<(), ValidationError> {
+    if  VersionReq::parse("0.1").unwrap().matches(v) {
+        return Ok(());
+    } else {
+        return Err(ValidationError::new(
+            "File version incompatible with semver 0.1",
+        ));
+    }
+}
+
+#[derive(Deserialize, Validate)]
+struct ConfigHeader {
+    #[validate(custom(function = "valid_version"))]
+    version: Version,
+}
+
+fn zero() -> f64 {
+    return 0.0;
+}
+
+fn left_zero() -> Either<f64, String> {
+    return Left(0.0);
+}
+
+#[derive(Deserialize)]
+struct DocTemplate {
+    tags: Vec<String>,
+    #[serde(default = "zero")]
+    order: f64,
+    output: Vec<DocTemplateElement>,
+}
+
+#[derive(Deserialize)]
+struct DocTemplateElement {
+    file: String,
+    #[serde(with = "either::serde_untagged", default = "left_zero")]
+    order: Either<f64, String>,
+    body: String,
+}
+
+enum TemplateError {
+    Mustache(mustache::Error),
+    Parse(ParseFloatError),
+}
+
+impl From<TemplateError> for SrcDocError {
+    fn from(value: TemplateError) -> Self {
+        return match value {
+            TemplateError::Parse(e) => SrcDocError::new(format!("Error parsing @order {}", e)),
+            TemplateError::Mustache(e) => SrcDocError::new(format!("Template parsing error {}", e)),
+        }
+    }
+}
+
+impl From<mustache::Error> for TemplateError {
+    fn from(value: mustache::Error) -> Self {
+        return TemplateError::Mustache(value);
+    }
+}
+
+impl From<ParseFloatError> for TemplateError {
+    fn from(value: ParseFloatError) -> Self {
+        return TemplateError::Parse(value);
+    }
+}
+
+impl DocTemplateElement {
+    fn apply(&self, doc: &DocData) -> Result<DocResult, TemplateError> {
+        let mut builder = MapBuilder::new();
+        for (key, val) in &doc.tags {
+            builder = builder.insert_str(key, val);
+        }
+        builder = builder.insert_str("__body__", &doc.body);
+        let data = builder.build();
+
+        let file: String = mustache::compile_str(&self.file)?.render_data_to_string(&data)?;
+        let order: f64 = match &self.order {
+            Left(n) => *n,
+            Right(str) => {
+                let n_str: String = mustache::compile_str(&str)?.render_data_to_string(&data)?;
+                match n_str.parse() {
+                    Ok(x) => x,
+                    Err(e) => {
+                        eprintln!("Error while evaluating @order {}: {}", &str, e);
+                        println!("{:?}", data);
+                        0.0
+                    }
+                }
             }
         };
+        let body: String = mustache::compile_str(&self.body)?.render_data_to_string(&data)?;
+        return Ok(DocResult { file, order, body });
+    }
+}
 
-        let mut sorted = items.clone();
-        sorted.sort_by(|a,b| a.partial_cmp(b).unwrap_or(Ordering::Less));
-        for (_, body) in sorted {
-            match writeln!(io, "{}", body) {
-                Err(e) => {
-                    eprintln!("Failed to write {}", e);
-                    std::process::exit(1);
+impl DocTemplate {
+    fn apply(&self, doc: &DocData) -> Result<Vec<DocResult>, TemplateError> {
+        if self.tags.iter().all(|tag| doc.tags.contains_key(tag)) {
+            return self.output.iter().map(|t| t.apply(&doc)).collect();
+        } else {
+            return Ok(Vec::new());
+        }
+    }
+}
+
+impl SrcDocConfig {
+    fn apply(
+        &self,
+        data: DocData,
+        results: &mut HashMap<String, Vec<(f64, String)>>,
+    ) -> Result<(), TemplateError> {
+
+        if let Some(x) = &self.template {
+            for template in x {
+                let applied = template.apply(&data)?;
+                if !applied.is_empty() {
+                    for r in applied {
+                        let items = results.entry(r.file).or_insert(Vec::new());
+                        items.push((r.order, r.body));
+                    }
+                    return Ok(());
                 }
-                Ok(_) => ()
+            }
+        }
+        return match DocResult::new(data) {
+            None => Ok(()),
+            Some(r) => {
+                let items = results.entry(r.file).or_insert(Vec::new());
+                items.push((r.order, r.body));
+                Ok(())
             }
         }
     }
@@ -163,9 +386,10 @@ impl<T: Iterator<Item = String>> Comments<T> {
         };
     }
 }
+
+#[derive(Debug)]
 struct CommentResult {
     value: String,
-    line: i64,
     last: bool,
 }
 
@@ -184,8 +408,7 @@ impl<T: Iterator<Item = String>> Iterator for Comments<T> {
         } else if self.in_comment && self.end_comment.is_match(value.as_str()) {
             let result = Some(CommentResult {
                 value: String::new(),
-                line: self.line_count,
-                last: true
+                last: true,
             });
             self.in_comment = false;
             self.line_count = 0;
@@ -198,23 +421,18 @@ impl<T: Iterator<Item = String>> Iterator for Comments<T> {
                 if let Some(cap_match) = capture.get(1) {
                     return Some(CommentResult {
                         value: String::from(cap_match.as_str()),
-                        line: self.line_count,
                         last: false,
                     });
                 }
             }
-            return Some(CommentResult {
-                value,
-                line: self.line_count,
-                last: false,
-            });
+            return Some(CommentResult { value, last: false });
         } else {
             return self.next();
         }
     }
 }
 
-// Docs ////////////////////////////////////////////////////////////////////////////////////
+// Parsed Docs /////////////////////////////////////////////////////////////////////////////
 
 struct DocIterator<T: Iterator<Item = String>> {
     comments: Comments<T>,
@@ -222,26 +440,10 @@ struct DocIterator<T: Iterator<Item = String>> {
     order_field: Regex,
 }
 
-impl fmt::Display for DocResult {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        return write!(f, "{}[{}]:\n{}\n", self.file, self.order, self.body);
-    }
-}
-
-struct DocResult {
-    file: String,
-    order: f64,
+#[derive(Debug)]
+struct DocData {
+    tags: HashMap<String, String>,
     body: String,
-}
-
-fn get_capture(x: &String, re: &Regex) -> Option<String> {
-    return match re.captures(x) {
-        Some(cap) => match cap.get(1) {
-            Some(cmatch) => Some(String::from(cmatch.as_str())),
-            None => None,
-        },
-        None => None,
-    };
 }
 
 impl<T: Iterator<Item = String>> DocIterator<T> {
@@ -252,67 +454,72 @@ impl<T: Iterator<Item = String>> DocIterator<T> {
             order_field,
         };
     }
-    fn read_file_field(&mut self) -> Option<String> {
-        // read @file (maybe turn into a function)
-        let mut comment = match self.comments.next() {
-            Some(c) => c,
-            None => return None,
-        };
-        let mut capture = get_capture(&comment.value, &self.file_field);
-        while comment.line != 1 || capture.is_none() {
-            comment = match self.comments.next() {
-                Some(c) => c,
-                None => return None,
-            };
-            capture = get_capture(&comment.value, &self.file_field);
-        }
-        return capture;
-    }
-
-    fn read_order_field(&mut self) -> Either<f64, String> {
-        let comment = match self.comments.next() {
-            Some(x) => x,
-            None => return Left(0.0),
-        };
-        match get_capture(&comment.value, &self.order_field) {
-            Some(num_str) => match num_str.parse() {
-                Ok(order) => return Left(order),
-                Err(_) => {
-                    println!("Non-numeric `@order` value `{}`, ignoring.", num_str);
-                    return Left(0.0);
-                }
-            },
-            None => return Right(comment.value),
-        };
-    }
 }
 
 impl<T: Iterator<Item = String>> Iterator for DocIterator<T> {
-    type Item = DocResult;
-    fn next(&mut self) -> Option<DocResult> {
+    type Item = DocData;
+    fn next(&mut self) -> Option<DocData> {
+        let tagr: Regex = Regex::new(r".*@(?<tag>\S+)\s+(?<value>.*)").unwrap();
         let mut body = String::new();
-
-        let file = match self.read_file_field() {
-            Some(f) => f,
-            None => return None,
-        };
-
-        let order_or_first_line = self.read_order_field();
-        let order = match order_or_first_line {
-            Left(x) => x,
-            Right(line) => {
-                body.push_str(&line);
-                body.push('\n');
-                0.0
-            }
-        };
+        let mut tags = HashMap::new();
+        let mut available_data = false;
 
         for comment in &mut self.comments {
-            if comment.last { break }
-            body.push_str(&comment.value);
-            body.push('\n');
+            if comment.last {
+                break;
+            }
+
+            if let Some(m) = tagr.captures(&comment.value) {
+                if &m["tag"] == "__body__" {
+                    eprintln!("The tag `__body__` is reserved.");
+                    std::process::exit(1);
+                }
+                tags.insert(String::from(&m["tag"]), String::from(&m["value"]));
+            } else {
+                available_data = true;
+                body.push_str(&comment.value);
+                body.push('\n');
+            }
         }
 
-        return Some(DocResult { file, order, body });
+        if available_data {
+            if !tags.contains_key("order") {
+                tags.insert("order".to_string(), "0.0".to_string());
+            }
+            return Some(DocData { tags, body });
+        } else {
+            return None;
+        }
+    }
+}
+
+// Processed Docs //////////////////////////////////////////////////////////////////////////
+
+#[derive(Debug, Deserialize, Clone)]
+struct DocResult {
+    file: String,
+    #[serde(default = "zero")]
+    order: f64,
+    body: String,
+}
+
+impl DocResult {
+    fn new(x: DocData) -> Option<DocResult> {
+        let file = match x.tags.get("file") {
+            None => return None,
+            Some(file) => String::from(file),
+        };
+        let order = match x.tags.get("order") {
+            None => 0.0,
+            Some(o) => match o.parse() {
+                Err(_) => 0.0,
+                Ok(o_value) => o_value,
+            },
+        };
+        return Some(DocResult {
+            file,
+            order,
+            body: x.body,
+        });
     }
 }
